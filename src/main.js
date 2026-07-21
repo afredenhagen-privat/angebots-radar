3805bcdcce9cad83d5d4688c0c044f350fb2d90c
Adrian Fredenhagen <adrian.fredenhagen@gmail.com>
2026-07-22 00:03:50 +0200

    feat(ui): vite + vue + tailwind base scaffold

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// src/main.js
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import router from './router.js'
+import App from './App.vue'
+import './style.css'
+
+createApp(App).use(createPinia()).use(router).mount('#app')
